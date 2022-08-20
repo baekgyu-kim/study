71dfb2a089ff3a3a6a68e082608860dfec77e6ac
centneuf <80656752+centneuf0109@users.noreply.github.com>
2022-08-20 17:09:13 +0900

    update walker

--- src/controllers/walkerController.js
+++ src/controllers/walkerController.js
@@ -5,10 +5,29 @@ export const getWalkerProfile = (req, res) => {
     return res.render("profile", { pageTitle: "프로필" });
 };
 
-export const getInfo = (req, res) => {
+export const getInfo = async (req, res) => {
+    const user_id = req.session.user._id;
+    const user = await User.findById(user_id);
+    const { walkerIntro, walkerDogSize } = user;
+    if (walkerIntro === undefined || walkerDogSize === undefined) {
+        const errorMessage =
+            "아직 선생님 정보가 등록되지 않았습니다. 선생님 정보를 먼저 등록해주세요.";
+        return res.render("walkerInfo", {
+            pageTitle: "선생님 정보",
+            errorMessage,
+        });
+    }
     return res.render("walkerInfo", { pageTitle: "선생님 정보" });
 };
 
 export const postInfo = (req, res) => {
+    return res.send("아직만드는중");
+};
+
+export const getEdit = (req, res) => {
+    return res.send("아직만드는중");
+};
+
+export const postEdit = (req, res) => {
     return res.send("아직만드는중");
 };
--- src/routers/walkerRouter.js
+++ src/routers/walkerRouter.js
@@ -4,11 +4,14 @@ import {
     getWalkerProfile,
     getInfo,
     postInfo,
+    getEdit,
+    postEdit,
 } from "../controllers/walkerController";
 
 const walkerRouter = express.Router();
 
 walkerRouter.route("/").get(getWalkerProfile);
 walkerRouter.route("/info").get(getInfo).post(postInfo);
+walkerRouter.route("/edit").get(getEdit).post(postEdit);
 
 export default walkerRouter;
