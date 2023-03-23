4c347db2107aaac8d4aefe49798702e2bc40c023
Baekgyu <centneuf0109@gmail.com>
2023-03-23 16:16:39 +0900

    add: initialize express server and mongoDB

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import express from "express";
+
+import "./config/db";
+import rootRouter from "./routers/rootRouter";
+
+const app = express();
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+    console.log(`✅${PORT}번 포트에 서버 연결됨`);
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/", rootRouter);
