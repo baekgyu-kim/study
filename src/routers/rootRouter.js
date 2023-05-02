7f8c640cea8b2f303bf24adf1073d1e43e44973e
Baekgyu <centneuf0109@gmail.com>
2023-05-02 15:01:28 +0900

    add: rootRouter.js

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,8 +1,19 @@
 import express from "express";
+
+import authRouter from "./authRouter";
+import stuRouter from "./stuRouter";
+import profRouter from "./profRouter";
+import sugangRouter from "./sugangRouter";
+
 import { getHome } from "../controllers/rootController";
 
 const rootRouter = express.Router();
 
 rootRouter.route("/").get(getHome);
+
+rootRouter.use("/auth", authRouter);
+rootRouter.use("/stu", stuRouter);
+rootRouter.use("/prof", profRouter);
+rootRouter.use("/sugang", sugangRouter);
 
 export default rootRouter;
