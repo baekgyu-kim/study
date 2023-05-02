cc6adf084eb502092af68985557abf0b56887816
Baekgyu <centneuf0109@gmail.com>
2023-05-02 15:01:03 +0900

    add: User.js

---
+++ src/models/User.js
@@ -0,0 +1,20 @@
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    id: { type: String, required: true },
+    pw: { type: String, required: true },
+    name: { type: String, required: true },
+    stuId: { type: String },
+    userType: { type: String, required: true },
+    lectureArr: [{ type: mongoose.Schema.Types.ObjectId, ref: "Lecture" }],
+});
+
+userSchema.pre("save", async function () {
+    if (this.isModified("pw")) {
+        this.pw = await bcrypt.hash(this.pw, 5);
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
