354f146711708d76ae591ad48a2bec29d81217a8
Baekgyu <centneuf0109@gmail.com>
2023-05-02 15:01:52 +0900

    add: middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -3,3 +3,19 @@ export const saveSessionToLocal = (req, res, next) => {
     res.locals.loggedInUser = req.session.loggedInUSer;
     next();
 };
+
+export const onlyLoggedIn = (req, res, next) => {
+    if (req.session.isLoggedIn) {
+        return next();
+    } else {
+        return res.redirect("/login");
+    }
+};
+
+export const onlyLoggedOut = (req, res, next) => {
+    if (!req.session.isLoggedIn) {
+        return next();
+    } else {
+        return res.redirect("/");
+    }
+};
