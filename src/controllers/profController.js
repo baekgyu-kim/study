e7040bab4dc564dc5829973db93d9e13cac949e1
Baekgyu <centneuf0109@gmail.com>
2023-08-09 17:40:18 +0900

    add: initialize profRouter, profController.js

--- src/controllers/profController.js
+++ src/controllers/profController.js
@@ -1,10 +1,18 @@
 import User from "../models/User";
 import Lecture from "../models/Lecture";
 
-export const getAllLectures = async (req, res) => {};
+export const getAllLectures = async (req, res) => {
+    const loggedInUSer = req.session.loggedInUser;
+};
 
 export const getNewLecture = async (req, res) => {};
 
+export const postNewLecture = async (req, res) => {};
+
 export const getOneLecture = async (req, res) => {};
 
+export const postOneNotice = async (req, res) => {};
+
 export const getAllStudents = async (req, res) => {};
+
+export const cancelOneStudent = async (req, res) => {};
--- src/routers/profRouter.js
+++ src/routers/profRouter.js
@@ -2,15 +2,31 @@ import express from "express";
 import {
     getAllLectures,
     getNewLecture,
+    postNewLecture,
     getOneLecture,
+    postOneNotice,
     getAllStudents,
+    cancelOneStudent,
 } from "../controllers/profController";
+import { onlyIsLoggedIn } from "../middleware";
 
 const profRouter = express.Router();
 
-profRouter.route("/lecture").get(getAllLectures);
-profRouter.route("/lecture/newLecture").get(getNewLecture);
-profRouter.route("/lecture/:id").get(getOneLecture);
-profRouter.route("/students").get(getAllStudents);
+profRouter.route("/lecture").all(onlyIsLoggedIn).get(getAllLectures);
+profRouter
+    .route("/lecture/newLecture")
+    .all(onlyIsLoggedIn)
+    .get(getNewLecture)
+    .post(postNewLecture);
+profRouter
+    .route("/lecture/:id")
+    .all(onlyIsLoggedIn)
+    .get(getOneLecture)
+    .post(postOneNotice);
+profRouter
+    .route("/student")
+    .all(onlyIsLoggedIn)
+    .get(getAllStudents)
+    .patch(cancelOneStudent);
 
 export default profRouter;
