401cd1ee0d08a784e1bd7afb70f3aaceef512592
Baekgyu <centneuf0109@gmail.com>
2023-08-09 16:51:59 +0900

    add: initialize profRouter, profController

---
+++ src/controllers/profController.js
@@ -0,0 +1,10 @@
+import User from "../models/User";
+import Lecture from "../models/Lecture";
+
+export const getAllLectures = async (req, res) => {};
+
+export const getNewLecture = async (req, res) => {};
+
+export const getOneLecture = async (req, res) => {};
+
+export const getAllStudents = async (req, res) => {};
--- src/routers/profRouter.js
+++ src/routers/profRouter.js
@@ -1,5 +1,16 @@
 import express from "express";
+import {
+    getAllLectures,
+    getNewLecture,
+    getOneLecture,
+    getAllStudents,
+} from "../controllers/profController";
 
 const profRouter = express.Router();
+
+profRouter.route("/lecture").get(getAllLectures);
+profRouter.route("/lecture/newLecture").get(getNewLecture);
+profRouter.route("/lecture/:id").get(getOneLecture);
+profRouter.route("/students").get(getAllStudents);
 
 export default profRouter;
