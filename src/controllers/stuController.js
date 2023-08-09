84abbd6c3411a790d4df5993c440c70e0a3ebc6c
Baekgyu <centneuf0109@gmail.com>
2023-08-09 17:41:28 +0900

    add: initialize stuRouter, controller.js

---
+++ src/controllers/stuController.js
@@ -0,0 +1,12 @@
+import Lecture from "../models/Lecture";
+import User from "../models/User";
+
+export const getSugang = (req, res) => {};
+
+export const postSugang = (req, res) => {};
+
+export const getAllLectures = (req, res) => {};
+
+export const getOneLecture = (req, res) => {};
+
+export const getUpdate = (req, res) => {};
--- src/routers/stuRouter.js
+++ src/routers/stuRouter.js
@@ -1,5 +1,19 @@
 import express from "express";
 
+import {
+    getSugang,
+    postSugang,
+    getAllLectures,
+    getOneLecture,
+    getUpdate,
+} from "../controllers/stuController";
+import { onlyIsLoggedIn } from "../middleware";
+
 const stuRouter = express.Router();
+
+stuRouter.route("/sugang").all(onlyIsLoggedIn).get(getSugang).post(postSugang);
+stuRouter.route("/lecture").all(onlyIsLoggedIn).get(getAllLectures);
+stuRouter.route("/lecture/:id").all(onlyIsLoggedIn).get(getOneLecture);
+stuRouter.route("/update").all(onlyIsLoggedIn).get(getUpdate);
 
 export default stuRouter;
