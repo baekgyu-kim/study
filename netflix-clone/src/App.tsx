3b257ac6701eee36500374117008fabb7e633c2a
Baekgyu <baekgyu.dev@gmail.com>
2024-01-16 17:59:36 +0900

    프로젝트 초기화

--- netflix-clone/src/App.tsx
+++ netflix-clone/src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Search from './Routes/Search';
+import Tv from './Routes/Tv';
+import Home from './Routes/Home';
+import Header from './Components/header';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+      <>
+          <Header />
+          <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/tv" element={<Tv />}></Route>
+              <Route path="/search" element={<Search />}></Route>
+          </Routes>
+      </>
   );
 }
 
